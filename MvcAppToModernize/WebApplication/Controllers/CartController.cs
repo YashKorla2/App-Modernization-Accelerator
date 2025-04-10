5c254f9f9bb4d367359ed614dd2ac10ae9dcb4f8
NoReply Amazon <no-reply@amazon.com>
2025-04-10 11:33:50 +0000

    Revert "[Commit] Iteration 462 #errors = 25 <= 25."
    This reverts commit 5c3f335555f99722c7baa9d5840f5360d77f55eb.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Services;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace WebApplication.Controllers
 {
@@ -29,14 +26,14 @@ namespace WebApplication.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
+        public ActionResult Delete(int id)
         {
             _cartService.DeleteCartItem(id);
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult Checkout(int[] selectedItems)
+        public ActionResult Checkout(int[] selectedItems)
         {
             if (selectedItems == null || selectedItems.Length == 0)
                 return RedirectToAction("Index");
