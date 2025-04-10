f86f42d4234806be1252f41d8515be525fa408d9
NoReply Amazon <no-reply@amazon.com>
2025-04-10 11:40:42 +0000

    [Commit] Iteration 479 #errors = 25 <= 25.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0246][31,11] The type or namespace name 'List<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -27,7 +27,7 @@ namespace WebApplication.Controllers
 
 public IActionResult Index(string searchTerm)
 {
-    System.Collections.Generic.IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
+    IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
         ? _productService.GetAllProducts()
         : _productService.SearchProducts(searchTerm);
     var cartItems = _cartService.GetCarts();
@@ -35,7 +35,7 @@ public IActionResult Index(string searchTerm)
     var viewModel = new ProductViewModel
     {
         Products = products,
-        CartItemCount = cartItems is System.Collections.Generic.ICollection<Cart> collection ? collection.Count : cartItems.Count(),
+        CartItemCount = cartItems is ICollection<Cart> collection ? collection.Count : cartItems.Count(),
         SearchTerm = searchTerm
     };
 
