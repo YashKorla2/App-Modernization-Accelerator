62ed89476a6c975b141ddf2b055a0cdbf518ddd8
NoReply Amazon <no-reply@amazon.com>
2025-04-10 11:12:40 +0000

    Revert "[Commit] Iteration 415 #errors = 25 <= 25."
    This reverts commit aa1d1534d31587ad97fb658e798d4d99b21aa6ec.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WebApplication.Controllers
 {
