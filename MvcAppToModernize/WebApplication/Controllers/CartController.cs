5b52c81ee2d7569a9e8eedc96c3ced0d3fdf708b
NoReply Amazon <no-reply@amazon.com>
2025-04-10 08:40:19 +0000

    Revert "[Commit] Iteration 80 #errors = 30 <= 30."
    This reverts commit 0f5093e02cb3797ea680d178c2242611b3169fc0.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,7 +1,6 @@
 using Services;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Linq;
+
 
 namespace WebApplication.Controllers
 {
@@ -13,8 +12,8 @@ namespace WebApplication.Controllers
         {
             _cartService = cartService;
         }
-
-        public IActionResult Index(string searchTerm)
+        
+        public ActionResult Index(string searchTerm)
         {
             var Carts = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetCarts()
@@ -27,20 +26,20 @@ namespace WebApplication.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
+        public ActionResult Delete(int id)
         {
             _cartService.DeleteCartItem(id);
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult Checkout(int[] selectedItems)
+        public ActionResult Checkout(int[] selectedItems)
         {
-            if (selectedItems == null || !selectedItems.Any())
-                return RedirectToAction(nameof(Index));
+            if (selectedItems == null || selectedItems.Length == 0)
+                return RedirectToAction("Index");
 
             _cartService.Checkout(selectedItems);
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
     }
 }
