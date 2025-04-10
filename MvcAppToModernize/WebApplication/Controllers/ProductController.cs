35c385958ffe90caba6de15b797951289a4e8b6a
NoReply Amazon <no-reply@amazon.com>
2025-04-10 09:41:20 +0000

    Revert "[Commit] Iteration 200 #errors = 30 <= 30."
    This reverts commit 3e5d0627ec57fc653c24269870139fad0a4eb6a6.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,10 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+
 
 namespace WebApplication.Controllers
 {
@@ -31,7 +30,7 @@ namespace WebApplication.Controllers
             var viewModel = new ProductViewModel
             {
                 Products = products,
-                CartItemCount = cartItems.Count()
+                CartItemCount = cartItems.Count
             };
 
             ViewBag.SearchTerm = searchTerm;
