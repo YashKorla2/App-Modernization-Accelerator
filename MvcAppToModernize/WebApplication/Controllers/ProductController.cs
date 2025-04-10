da2d6c470521895f6780f45d11090b38e927fa00
NoReply Amazon <no-reply@amazon.com>
2025-04-10 11:24:08 +0000

    Revert "[Commit] Iteration 439 #errors = 25 <= 25."
    This reverts commit ce1fa55eb69547afa7f5b38d3b2a713d7d4076a9.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -4,7 +4,6 @@ using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication.Controllers
 {
@@ -25,9 +24,6 @@ namespace WebApplication.Controllers
             _productService = productService ?? throw new ArgumentNullException(nameof(productService));
             _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
         }
-
-        // Helper method to ensure Object type is used
-        private object GetObject() => new object();
 
 public IActionResult Index(string searchTerm)
 {
