93831bcbaa09aa902da6ab31404b13ce7398376d
NoReply Amazon <no-reply@amazon.com>
2025-04-10 12:18:19 +0000

    [Commit] Iteration 565 #errors = 24 <= 24.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0518][49,27] Predefined type 'System.Int32' is not defined or imported``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,10 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
-using Models;
-using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
 
 namespace WebApplication.Controllers
 {
