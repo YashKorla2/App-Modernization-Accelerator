01313a2c52c0ef3b1aa3b506f755e518409293a6
NoReply Amazon <no-reply@amazon.com>
2025-04-10 09:24:42 +0000

    Revert "[Commit] Iteration 159 #errors = 30 <= 30."
    This reverts commit 28a878198aac619b8e2bf57288f5be78e188eef4.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,32 +1,31 @@
 using System.Collections.Generic;
 using Services;
 using Microsoft.AspNetCore.Mvc;
-using Models;
 
 
 namespace WebApplication.Controllers
 {
-public class OrderController : Controller
-{
-    private readonly ICartService _cartService;
-
-    public OrderController(ICartService cartService)
+    public class OrderController : Controller
     {
-        _cartService = cartService;
+        private readonly ICartService _cartService;
+
+        public OrderController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public ActionResult Index(string searchTerm)
+        {
+            var orders = string.IsNullOrEmpty(searchTerm)
+                ? _cartService.GetOrders()
+                : _cartService.SearchOrders(searchTerm);
+            
+            var orderCount = orders.Count;
+
+            ViewBag.OrderCount = orderCount;
+            ViewBag.SearchTerm = searchTerm;
+
+            return View(orders);
+        }
     }
-
-    public ActionResult Index(string searchTerm)
-    {
-        List<Order> orders = string.IsNullOrEmpty(searchTerm)
-            ? _cartService.GetOrders()
-            : _cartService.SearchOrders(searchTerm);
-
-        int orderCount = orders.Count;
-
-        ViewBag.OrderCount = orderCount;
-        ViewBag.SearchTerm = searchTerm;
-
-        return View(orders);
-    }
-}
 }
