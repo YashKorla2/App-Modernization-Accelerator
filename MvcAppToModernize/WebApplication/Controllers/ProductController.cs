cb523b886ab2d49610ef150de32c5e09b942aee0
NoReply Amazon <no-reply@amazon.com>
2025-04-10 12:15:38 +0000

    Revert "[Commit] Iteration 558 #errors = 24 <= 24."
    This reverts commit 9aa5308b3750f5b9c79c497d592299e3e3280cc3.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -4,6 +4,7 @@ using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System;
 
 namespace WebApplication.Controllers
 {
@@ -30,12 +31,12 @@ public IActionResult Index(string searchTerm)
     System.Collections.Generic.IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
         ? _productService.GetAllProducts()
         : _productService.SearchProducts(searchTerm);
-    IEnumerable<Cart> cartItems = _cartService.GetCarts();
+    var cartItems = _cartService.GetCarts();
 
     var viewModel = new ProductViewModel
     {
         Products = products,
-        CartItemCount = cartItems is ICollection<Cart> collection ? collection.Count : cartItems.Count(),
+        CartItemCount = cartItems is System.Collections.Generic.ICollection<Cart> collection ? collection.Count : cartItems.Count(),
         SearchTerm = searchTerm
     };
 
