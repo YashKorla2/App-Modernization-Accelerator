4c93b63dd4c45dcb8f3522df3f050326551d3f48
NoReply Amazon <no-reply@amazon.com>
2025-04-10 08:59:18 +0000

    Revert "[Commit] Iteration 117 #errors = 30 <= 30."
    This reverts commit 10bfd8f08d451ed1ae6258aabfe73fccce70fe3a.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,10 +1,9 @@
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
+
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+
 
 namespace WebApplication.Controllers
 {
