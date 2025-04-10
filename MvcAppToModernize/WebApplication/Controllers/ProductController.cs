492341c6927fb6dd8395d462f8d479e2047c0332
NoReply Amazon <no-reply@amazon.com>
2025-04-10 11:56:47 +0000

    Revert "[Commit] Iteration 516 #errors = 25 <= 25."
    This reverts commit e41b727214d71fb034eae0ea00fd4bc6ae243f8c.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -4,7 +4,6 @@ using Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication.Controllers
 {
@@ -25,9 +24,6 @@ namespace WebApplication.Controllers
             _productService = productService ?? throw new ArgumentNullException(nameof(productService));
             _cartService = cartService ?? throw new ArgumentNullException(nameof(cartService));
         }
-
-        // Explicitly declare int to ensure it's recognized
-        private static int IntegerExample => 0;
 
 public IActionResult Index(string searchTerm)
 {
