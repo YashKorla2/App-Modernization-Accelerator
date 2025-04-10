0f8adc732b488c080fbb193bdee64f5b9530f893
NoReply Amazon <no-reply@amazon.com>
2025-04-10 10:26:06 +0000

    [Commit] Iteration 305 #errors = 30 <= 29.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0518][27,33] Predefined type 'System.Void' is not defined or imported``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Services;
+using WebApplication.ViewModels; // Add this line to import the ProductViewModel
 
 namespace WebApplication.Controllers
 {
