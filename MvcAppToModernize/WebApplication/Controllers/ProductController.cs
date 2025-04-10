ae2c6ed9fc1f556c32c76850309ea45d89532b0c
NoReply Amazon <no-reply@amazon.com>
2025-04-10 10:02:54 +0000

    Revert "[Commit] Iteration 249 #errors = 30 <= 30."
    This reverts commit 27c421e922ec1f1d197d8a4e2eec7c2ca8e191ba.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,8 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Services;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace WebApplication.Controllers
 {
@@ -19,7 +17,7 @@ namespace WebApplication.Controllers
 
         public IActionResult Index(string searchTerm)
         {
-            IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
+            var products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
             var cartItems = _cartService.GetCarts();
@@ -27,7 +25,7 @@ namespace WebApplication.Controllers
             var viewModel = new ProductViewModel
             {
                 Products = products,
-                CartItemCount = cartItems.Count()
+                CartItemCount = cartItems.Count
             };
 
             ViewBag.SearchTerm = searchTerm;
