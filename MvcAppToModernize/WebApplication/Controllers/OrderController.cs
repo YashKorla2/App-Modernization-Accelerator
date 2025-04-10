15ee7fd31e62280ba5e533cd558544c5827bca7a
NoReply Amazon <no-reply@amazon.com>
2025-04-10 08:00:32 +0000

    Revert "[Commit] Iteration 8 #errors = 31 <= 30."
    This reverts commit 38f686a16132327fd60e7aa4be762a073648b68d.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,11 +16,11 @@ namespace WebApplication.Controllers
 
         public ActionResult Index(string searchTerm)
         {
-            IEnumerable<Order> orders = string.IsNullOrEmpty(searchTerm)
+            var orders = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetOrders()
                 : _cartService.SearchOrders(searchTerm);
-
-            int orderCount = orders.Count();
+            
+            var orderCount = orders.Count;
 
             ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
