d15de388997a6fe6fbb405e60f3bb19b6e3e1f98
NoReply Amazon <no-reply@amazon.com>
2025-04-14 12:42:16 +0000

    Revert "[Commit] Iteration 418 #errors = 24 <= 22."
    This reverts commit 5efd4f02c500dfc60b232de6d932a222255dfca2.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Services;
 
@@ -12,15 +15,17 @@ namespace WebApplication.Controllers
             _cartService = cartService;
         }
 
-        public IActionResult Index(string searchTerm)
+        public ActionResult Index(string searchTerm)
         {
             IEnumerable<object> ordersEnumerable = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetOrders()
                 : _cartService.SearchOrders(searchTerm);
 
-            List<object> orders = ordersEnumerable.ToList();
+            var orders = ordersEnumerable.ToList();
 
-            ViewBag.OrderCount = orders.Count;
+            var orderCount = orders.Count;
+
+            ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
 
             return View(orders);
