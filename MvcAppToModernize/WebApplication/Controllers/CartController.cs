6d7ba570cc8d9e16ae588d34e4c07c08385ca122
NoReply Amazon <no-reply@amazon.com>
2025-04-14 10:07:53 +0000

    Revert "[Commit] Iteration 269 #errors = 22 <= 22."
    This reverts commit 2f780d1d3fb5c25a3f85727325d4b411a449d4c1.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 using Services;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace WebApplication.Controllers
 {
     public class CartController : Controller
@@ -11,34 +12,34 @@ namespace WebApplication.Controllers
         {
             _cartService = cartService;
         }
-
-        public IActionResult Index(string searchTerm)
+        
+        public ActionResult Index(string searchTerm)
         {
-            var carts = string.IsNullOrEmpty(searchTerm)
+            var Carts = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetCarts()
                 : _cartService.SearchCart(searchTerm);
 
-            ViewData["SearchTerm"] = searchTerm;
+            ViewBag.SearchTerm = searchTerm;
 
-            return View(carts);
+            return View(Carts);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Delete(int id)
+        public ActionResult Delete(int id)
         {
             _cartService.DeleteCartItem(id);
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public IActionResult Checkout(int[] selectedItems)
+        public ActionResult Checkout(int[] selectedItems)
         {
             if (selectedItems == null || selectedItems.Length == 0)
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index");
 
             _cartService.Checkout(selectedItems);
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction("Index");
         }
     }
 }
