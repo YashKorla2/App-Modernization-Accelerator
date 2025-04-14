e2b3bb7f588edabc9f9533536bb7611667d7e774
NoReply Amazon <no-reply@amazon.com>
2025-04-14 12:06:39 +0000

    [Commit] Iteration 386 #errors = 22 <= 22.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/CartController.cs][CS0518][41,13] Predefined type 'System.Void' is not defined or imported``.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,45 +1,43 @@
 using Services;
 using Microsoft.AspNetCore.Mvc;
 
+namespace WebApplication.Controllers;
 
-namespace WebApplication.Controllers
+public class CartController : Controller
 {
-    public class CartController : Controller
+    private readonly ICartService _cartService;
+
+    public CartController(ICartService cartService)
     {
-        private readonly ICartService _cartService;
+        _cartService = cartService;
+    }
 
-        public CartController(ICartService cartService)
-        {
-            _cartService = cartService;
-        }
-        
-        public ActionResult Index(string searchTerm)
-        {
-            var Carts = string.IsNullOrEmpty(searchTerm)
-                ? _cartService.GetCarts()
-                : _cartService.SearchCart(searchTerm);
+    public IActionResult Index(string? searchTerm)
+    {
+        var carts = string.IsNullOrEmpty(searchTerm)
+            ? _cartService.GetCarts()
+            : _cartService.SearchCart(searchTerm);
 
-            ViewBag.SearchTerm = searchTerm;
+        ViewBag.SearchTerm = searchTerm;
 
-            return View(Carts);
-        }
+        return View(carts);
+    }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id)
-        {
-            _cartService.DeleteCartItem(id);
-            return RedirectToAction("Index");
-        }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id)
+    {
+        _cartService.DeleteCartItem(id);
+        return RedirectToAction(nameof(Index));
+    }
 
-        [HttpPost]
-        public ActionResult Checkout(int[] selectedItems)
-        {
-            if (selectedItems == null || selectedItems.Length == 0)
-                return RedirectToAction("Index");
+    [HttpPost]
+    public IActionResult Checkout(int[] selectedItems)
+    {
+        if (selectedItems == null || selectedItems.Length == 0)
+            return RedirectToAction(nameof(Index));
 
-            _cartService.Checkout(selectedItems);
-            return RedirectToAction("Index");
-        }
+        _cartService.Checkout(selectedItems);
+        return RedirectToAction(nameof(Index));
     }
 }
