1932f279dba4d02bb958250443450154c8d3d09a
NoReply Amazon <no-reply@amazon.com>
2025-04-14 08:07:31 +0000

    [Commit] Iteration 103 #errors = 30 <= 30.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/CartController.cs][CS0518][41,13] Predefined type 'System.Void' is not defined or imported``.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Services;
 using Microsoft.AspNetCore.Mvc;
-
 
 namespace WebApplication.Controllers
 {
@@ -12,8 +15,8 @@ namespace WebApplication.Controllers
         {
             _cartService = cartService;
         }
-        
-        public ActionResult Index(string searchTerm)
+
+        public IActionResult Index(string searchTerm)
         {
             var Carts = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetCarts()
@@ -26,14 +29,14 @@ namespace WebApplication.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int id)
         {
             _cartService.DeleteCartItem(id);
             return RedirectToAction("Index");
         }
 
         [HttpPost]
-        public ActionResult Checkout(int[] selectedItems)
+        public IActionResult Checkout(int[] selectedItems)
         {
             if (selectedItems == null || selectedItems.Length == 0)
                 return RedirectToAction("Index");
