8b4ec848d58b2add7492509c8605ebe058d830db
NoReply Amazon <no-reply@amazon.com>
2025-04-14 08:59:56 +0000

    Revert "[Commit] Iteration 166 #errors = 27 <= 27."
    This reverts commit d53bd3fe5cab53b441c35d3d49129c0b0ae4e48a.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -35,7 +35,7 @@ namespace WebApplication.Controllers
             IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
-            System.Collections.Generic.List<Cart> cartItems = _cartService.GetCarts().ToList();
+            List<Cart> cartItems = _cartService.GetCarts().ToList();
 
             var viewModel = new ProductViewModel
             {
