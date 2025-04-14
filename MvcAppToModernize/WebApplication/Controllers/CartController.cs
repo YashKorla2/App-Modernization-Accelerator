5cbe14e29a20fc9fbc49c9bdad90a8e440db9dff
NoReply Amazon <no-reply@amazon.com>
2025-04-14 10:04:28 +0000

    [Commit] Iteration 260 #errors = 22 <= 22.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/CartController.cs][CS0246][19,19] The type or namespace name 'List<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 using Services;
 using Microsoft.AspNetCore.Mvc;
-
+using System.Collections.Generic;
+using Models; // Assuming Cart is in the Models namespace
 
 namespace WebApplication.Controllers
 {
@@ -15,13 +16,13 @@ namespace WebApplication.Controllers
         
         public ActionResult Index(string searchTerm)
         {
-            var Carts = string.IsNullOrEmpty(searchTerm)
+            IEnumerable<object> carts = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetCarts()
                 : _cartService.SearchCart(searchTerm);
 
             ViewBag.SearchTerm = searchTerm;
 
-            return View(Carts);
+            return View(carts);
         }
 
         [HttpPost]
