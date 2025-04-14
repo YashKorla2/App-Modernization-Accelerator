a52798ab697d134a81f89a3df84ab45166e8d55c
NoReply Amazon <no-reply@amazon.com>
2025-04-14 07:58:46 +0000

    Revert "[Commit] Iteration 84 #errors = 31 <= 31."
    This reverts commit 3e2d09b5e5a68e13b2a4502f1081f9f6fe8a054a.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -24,14 +24,14 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet]
-        public ActionResult<Models.ProductViewModel> Index(string searchTerm)
+        public ActionResult<ProductViewModel> Index(string searchTerm)
         {
-            System.Collections.Generic.IEnumerable<Models.Product> products = string.IsNullOrEmpty(searchTerm)
+            IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
-            System.Collections.Generic.IEnumerable<Models.Cart> cartItems = _cartService.GetCarts();
+            IEnumerable<Cart> cartItems = _cartService.GetCarts();
 
-            var viewModel = new Models.ProductViewModel
+            var viewModel = new ProductViewModel
             {
                 Products = products,
                 CartItemCount = cartItems.Count()
@@ -44,7 +44,7 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Models.Product> Details(int id)
+        public ActionResult<Product> Details(int id)
         {
             var product = _productService.GetProductById(id);
             if (product == null)
@@ -61,7 +61,7 @@ namespace WebApplication.Controllers
         }
 
         [HttpPost("create")]
-        public ActionResult<Models.Product> Create(Models.Product product)
+        public ActionResult<Product> Create(Product product)
         {
             if (ModelState.IsValid)
             {
@@ -72,7 +72,7 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet("edit/{id}")]
-        public ActionResult<Models.Product> Edit(int id)
+        public ActionResult<Product> Edit(int id)
         {
             var product = _productService.GetProductById(id);
             if (product == null)
@@ -83,7 +83,7 @@ namespace WebApplication.Controllers
         }
 
         [HttpPost("edit")]
-        public ActionResult<Models.Product> Edit(Models.Product product)
+        public ActionResult<Product> Edit(Product product)
         {
             if (ModelState.IsValid)
             {
@@ -94,7 +94,7 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet("delete/{id}")]
-        public ActionResult<Models.Product> Delete(int id)
+        public ActionResult<Product> Delete(int id)
         {
             var product = _productService.GetProductById(id);
             if (product == null)
