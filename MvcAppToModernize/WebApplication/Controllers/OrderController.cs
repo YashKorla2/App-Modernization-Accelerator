39ab7c3cb507eee3ed167ad7a1874fcc0b5ed79a
NoReply Amazon <no-reply@amazon.com>
2025-04-14 12:36:28 +0000

    Revert "[Commit] Iteration 406 #errors = 23 <= 23."
    This reverts commit 1c88beb9a09cca00c7da30960ffdf3bf1fc875c5.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,8 +1,7 @@
-using System;
+using Services;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Services;
 
 namespace WebApplication.Controllers
 {
