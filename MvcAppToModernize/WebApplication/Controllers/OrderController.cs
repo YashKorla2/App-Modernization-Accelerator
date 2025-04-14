c494511302eb7387b8bf56ee03d4ff24642c9487
NoReply Amazon <no-reply@amazon.com>
2025-04-14 10:35:38 +0000

    Revert "[Commit] Iteration 282 #errors = 22 <= 22."
    This reverts commit d5e76b2b174175376a9be7f2bd9671b8a8f42b7c.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -19,8 +19,8 @@ namespace WebApplication.Controllers
             var orders = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetOrders()
                 : _cartService.SearchOrders(searchTerm);
-
-            var orderCount = orders.Count();
+            
+            var orderCount = orders.Count;
 
             ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
