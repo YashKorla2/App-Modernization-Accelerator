b9d15d5f689dc8bac4ba814e4f67d7bda87daf0d
NoReply Amazon <no-reply@amazon.com>
2025-04-14 10:35:18 +0000

    Revert "[Commit] Iteration 281 #errors = 22 <= 22."
    This reverts commit 5c8e643956da1ee1976049ecc07027b43e31f35c.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 
 
 namespace WebApplication.Controllers
@@ -17,11 +16,11 @@ namespace WebApplication.Controllers
 
         public ActionResult Index(string searchTerm)
         {
-            IEnumerable<object> orders = string.IsNullOrEmpty(searchTerm)
+            var orders = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetOrders()
                 : _cartService.SearchOrders(searchTerm);
-
-            var orderCount = orders.Count();
+            
+            var orderCount = orders.Count;
 
             ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
