bfe8b57ce8a38b03c63c5ac14c32ff4f31295647
NoReply Amazon <no-reply@amazon.com>
2025-04-14 12:52:56 +0000

    Revert "[Commit] Iteration 443 #errors = 22 <= 22."
    This reverts commit c8080eae2f809c19634e771d32309726d6242899.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using System.Net;
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
