dd79b945332f7194d1d8e0d6b1c9646688d926f5
NoReply Amazon <no-reply@amazon.com>
2025-04-15 09:59:30 +0000

    [Commit] Iteration 407 #errors = 21 <= 21.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0246][36,29] The type or namespace name 'List<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -8,11 +8,11 @@ using System.Threading.Tasks;
 
 namespace WebApplication.Controllers
 {
-    public class ProductViewModel
-    {
-        public List<Product> Products { get; set; }
-        public int CartItemCount { get; set; }
-    }
+public class ProductViewModel
+{
+    public System.Collections.Generic.List<Product> Products { get; set; }
+    public int CartItemCount { get; set; }
+}
 
     [ApiController]
     [Route("[controller]")]
@@ -35,11 +35,11 @@ namespace WebApplication.Controllers
                 : _productService.SearchProducts(searchTerm);
             var cartItems = _cartService.GetCarts();
 
-            var viewModel = new ProductViewModel
-            {
-                Products = products.ToList(),
-                CartItemCount = cartItems.Count()
-            };
+var viewModel = new ProductViewModel
+{
+    Products = new System.Collections.Generic.List<Product>(products),
+    CartItemCount = cartItems.Count()
+};
 
             return viewModel;
         }
