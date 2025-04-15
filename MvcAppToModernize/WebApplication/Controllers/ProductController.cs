58936067f10449501b09d9e166fa6afd41e26029
NoReply Amazon <no-reply@amazon.com>
2025-04-15 11:35:30 +0000

    Revert "[Commit] Iteration 550 #errors = 21 <= 21."
    This reverts commit 5c0c16dec16673bd6e57d6632045209faccdce38.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System;
 using System.Linq;
-using System; // Explicitly adding System namespace again
 
 namespace WebApplication.Controllers
 {
