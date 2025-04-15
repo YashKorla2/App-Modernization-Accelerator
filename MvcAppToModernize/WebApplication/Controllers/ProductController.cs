237b119a4a70604f483ee5140456900e0fda321a
NoReply Amazon <no-reply@amazon.com>
2025-04-15 08:04:12 +0000

    Revert "[Commit] Iteration 148 #errors = 22 <= 22."
    This reverts commit 4a02a130534b8cf08713eac8f57eda351d530551.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -6,11 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
-public class ProductViewModel
-{
-    public IEnumerable<Product> Products { get; set; }
-    public int CartItemCount { get; set; }
-}
+    public class ProductViewModel
+    {
+        public List<Product> Products { get; set; }
+        public int CartItemCount { get; set; }
+    }
 
     [ApiController]
     [Route("[controller]")]
@@ -35,11 +35,11 @@ public class ProductViewModel
                 : _productService.SearchProducts(searchTerm);
             var cartItems = _cartService.GetCarts();
 
-var viewModel = new ProductViewModel
-{
-    Products = products,
-    CartItemCount = cartItems.Count
-};
+            var viewModel = new ProductViewModel
+            {
+                Products = products.ToList(),
+                CartItemCount = cartItems.Count
+            };
 
             return Ok(viewModel);
         }
