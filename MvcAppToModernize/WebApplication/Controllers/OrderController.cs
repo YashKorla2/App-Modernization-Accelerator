64c65aa85e58a459f7f3ff2335df309494b04702
NoReply Amazon <no-reply@amazon.com>
2025-04-15 08:54:15 +0000

    [Commit] Iteration 265 #errors = 21 <= 21.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/OrderController.cs][CS0246][22,19] The type or namespace name 'List<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,8 +1,8 @@
+using System;
 using System.Collections.Generic;
 using Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-
 
 namespace WebApplication.Controllers
 {
@@ -17,11 +17,11 @@ namespace WebApplication.Controllers
 
         public ActionResult Index(string searchTerm)
         {
-            IEnumerable<object> orders = string.IsNullOrEmpty(searchTerm)
+            System.Collections.Generic.IEnumerable<object> orders = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetOrders()
                 : _cartService.SearchOrders(searchTerm);
 
-            var orderCount = orders.Count();
+            int orderCount = orders.Count();
 
             ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
