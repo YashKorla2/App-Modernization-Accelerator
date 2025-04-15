6d65cc1bebee0dc1affe0332697603c4787c4c2b
NoReply Amazon <no-reply@amazon.com>
2025-04-15 08:55:05 +0000

    Revert "[Commit] Iteration 266 #errors = 21 <= 21."
    This reverts commit 363811021795c25e3f80beb9c81e54d6a75c516c.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections.Generic;
 using Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+
 
 namespace WebApplication.Controllers
 {
@@ -15,13 +15,13 @@ namespace WebApplication.Controllers
             _cartService = cartService;
         }
 
-        public IActionResult Index(string searchTerm)
+        public ActionResult Index(string searchTerm)
         {
             IEnumerable<object> orders = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetOrders()
                 : _cartService.SearchOrders(searchTerm);
 
-            int orderCount = orders.Count();
+            var orderCount = orders.Count();
 
             ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
