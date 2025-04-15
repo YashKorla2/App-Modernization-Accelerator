62903a1fbd6b552ebf63c9b8272a5deb3d5aa741
NoReply Amazon <no-reply@amazon.com>
2025-04-15 11:03:15 +0000

    Revert "[Commit] Iteration 532 #errors = 21 <= 21."
    This reverts commit b56f62ae60a2c1c6a0a6d321dc27b474779e992d.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -9,7 +9,7 @@ namespace WebApplication.Controllers
 {
     public class ProductViewModel
     {
-        public System.Collections.Generic.List<Product> Products { get; set; }
+        public List<Product> Products { get; set; }
         public int CartItemCount { get; set; }
     }
 
@@ -29,7 +29,7 @@ namespace WebApplication.Controllers
         [HttpGet]
         public ActionResult<ProductViewModel> Index(string? searchTerm)
         {
-            System.Collections.Generic.IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
+            IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
             var cartItems = _cartService.GetCarts();
