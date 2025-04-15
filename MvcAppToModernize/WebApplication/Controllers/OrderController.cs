5ddfdef821fd0b1dfdf9bea450c05e2a0baf3ede
NoReply Amazon <no-reply@amazon.com>
2025-04-15 10:45:42 +0000

    Revert "[Commit] Iteration 504 #errors = 21 <= 21."
    This reverts commit 5570b292de817c4b35f8c575c460ea00b8b74ec4.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,8 +1,8 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
 using Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
 
 namespace WebApplication.Controllers
 {
