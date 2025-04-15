b365a4b60692b5e7e6f56bdf10225657d363f198
NoReply Amazon <no-reply@amazon.com>
2025-04-15 07:48:25 +0000

    Revert "[Commit] Iteration 111 #errors = 30 <= 30."
    This reverts commit 68e005d81ae607279ee3433730c23c37c6d2615b.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -31,12 +31,12 @@ namespace WebApplication.Controllers
             var products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
-            IEnumerable<Cart> cartItems = _cartService.GetCarts();
+            var cartItems = _cartService.GetCarts();
 
             var viewModel = new ProductViewModel
             {
                 Products = products,
-                CartItemCount = cartItems.Count()
+                CartItemCount = cartItems.Count
             };
 
             return viewModel;
