a38c2a60b81e1a6bce5e58aa47918179485d233b
NoReply Amazon <no-reply@amazon.com>
2025-04-15 08:02:32 +0000

    Revert "[Commit] Iteration 144 #errors = 23 <= 27."
    This reverts commit 21a9f3f70093f0c374bf4c7641787ee90c5950fa.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Net;
 
 namespace WebApplication.Controllers
 {
@@ -54,58 +58,70 @@ namespace WebApplication.Controllers
             return Ok(product);
         }
 
-    [HttpPost]
-    public IActionResult Create([FromBody] Product product)
-    {
-        if (ModelState.IsValid)
+        public ActionResult Create()
         {
-            _productService.AddProduct(product);
-            return CreatedAtAction(nameof(Details), new { id = product.Id }, product);
-        }
-        return BadRequest(ModelState);
-    }
-
-    [HttpPut("{id}")]
-    public IActionResult Edit(int id, [FromBody] Product product)
-    {
-        var existingProduct = _productService.GetProductById(id);
-        if (existingProduct == null)
-        {
-            return NotFound();
+            return View();
         }
 
-        if (ModelState.IsValid)
+        [HttpPost]
+        public ActionResult Create(Product product)
         {
-            _productService.UpdateProduct(product);
-            return NoContent();
-        }
-        return BadRequest(ModelState);
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-        var product = _productService.GetProductById(id);
-        if (product == null)
-        {
-            return NotFound();
+            if (ModelState.IsValid)
+            {
+                _productService.AddProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
         }
 
-        _productService.DeleteProduct(id);
-        return NoContent();
-    }
-
-    [HttpPost("AddToCart")]
-    public IActionResult AddToCart(int productId, int quantity = 1)
-    {
-        var product = _productService.GetProductById(productId);
-        if (product == null)
+        public ActionResult Edit(int id)
         {
-            return NotFound();
+            var product = _productService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
         }
 
-        _cartService.AddProductToCart(product, quantity);
-        return Ok();
-    }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _productService.UpdateProduct(product);
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var product = _productService.GetProductById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _productService.DeleteProduct(id);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int productId, int quantity = 1)
+        {
+            var product = _productService.GetProductById(productId);
+            if (product != null)
+            {
+                _cartService.AddProductToCart(product, quantity);
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
