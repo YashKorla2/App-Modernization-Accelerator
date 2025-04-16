7bab179ca6ff1b8df2e95e54bae38fa354d7e1d6
NoReply Amazon <no-reply@amazon.com>
2025-04-16 15:47:33 +0000

    Revert "[Commit] Iteration 593 #errors = 21 <= 21."
    This reverts commit dd1accd41f95591ee701b474ce4e60b9a046d338.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Services;
@@ -19,8 +18,8 @@ namespace WebApplication.Controllers
         public ActionResult Index(string searchTerm)
         {
             IEnumerable<object> orders = string.IsNullOrEmpty(searchTerm)
-                ? _cartService.GetOrders().ToList()
-                : _cartService.SearchOrders(searchTerm).ToList();
+                ? _cartService.GetOrders()
+                : _cartService.SearchOrders(searchTerm);
 
             var orderCount = orders.Count();
 
