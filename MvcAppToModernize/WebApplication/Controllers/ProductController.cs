d765677d0cb5660288d55495054ad29e91522516
NoReply Amazon <no-reply@amazon.com>
2025-04-16 13:52:46 +0000

    [Commit] Iteration 392 #errors = 21 <= 21.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0518][70,17] Predefined type 'System.Object' is not defined or imported``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,3 +1,4 @@
+using System;
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
@@ -19,21 +20,21 @@ namespace WebApplication.Controllers
         }
 
         [HttpGet]
-        public ActionResult<object> Index(string searchTerm)
+    public ActionResult<Object> Index(string searchTerm)
+    {
+        IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
+            ? _productService.GetAllProducts()
+            : _productService.SearchProducts(searchTerm);
+        var cartItems = _cartService.GetCarts();
+
+        var viewModel = new
         {
-            IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
-                ? _productService.GetAllProducts()
-                : _productService.SearchProducts(searchTerm);
-            var cartItems = _cartService.GetCarts();
+            Products = products,
+            CartItemCount = cartItems.Count()
+        };
 
-            var viewModel = new
-            {
-                Products = products,
-                CartItemCount = cartItems.Count()
-            };
-
-            return Ok(viewModel);
-        }
+        return Ok(viewModel);
+    }
 
         [HttpGet("{id}")]
         public ActionResult<Product> Details(int id)
