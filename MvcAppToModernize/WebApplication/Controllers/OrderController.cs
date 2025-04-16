de7fd8074659815e82a31d2ef320d3a84951e1ba
NoReply Amazon <no-reply@amazon.com>
2025-04-16 13:47:34 +0000

    Revert "[Commit] Iteration 381 #errors = 21 <= 21."
    This reverts commit 933df0c9cf6854037aac3c4e8f199fc4cc532d12.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -17,11 +17,11 @@ namespace WebApplication.Controllers
 
         public ActionResult Index(string searchTerm)
         {
-            List<object> orders = string.IsNullOrEmpty(searchTerm)
-                ? _cartService.GetOrders().ToList()
-                : _cartService.SearchOrders(searchTerm).ToList();
+            IEnumerable<object> orders = string.IsNullOrEmpty(searchTerm)
+                ? _cartService.GetOrders()
+                : _cartService.SearchOrders(searchTerm);
 
-            var orderCount = orders.Count;
+            var orderCount = orders.Count();
 
             ViewBag.OrderCount = orderCount;
             ViewBag.SearchTerm = searchTerm;
