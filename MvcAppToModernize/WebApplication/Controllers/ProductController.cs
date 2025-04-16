907a519052ce347597ba83863dd9aa37a501eec4
NoReply Amazon <no-reply@amazon.com>
2025-04-16 15:30:37 +0000

    Revert "[Commit] Iteration 558 #errors = 21 <= 21."
    This reverts commit fe60f0f92652e7a0b29582d43e088e49ee4cd277.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Models;
 using Services;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace WebApplication.Controllers
 {
@@ -26,7 +24,7 @@ namespace WebApplication.Controllers
             IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
-            IEnumerable<object> cartItems = _cartService.GetCarts();
+            var cartItems = _cartService.GetCarts();
 
             var viewModel = new
             {
