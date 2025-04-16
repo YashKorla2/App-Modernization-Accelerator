aa9ee127d7e7897cc2a0d115cd0ddb8084331157
NoReply Amazon <no-reply@amazon.com>
2025-04-16 14:17:34 +0000

    Revert "[Commit] Iteration 447 #errors = 21 <= 21."
    This reverts commit e7fecb61f2d75d90dc41cdfdb9c02bf7034b017c.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Models;
 using Services;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace WebApplication.Controllers
 {
@@ -30,7 +28,7 @@ namespace WebApplication.Controllers
 
             var viewModel = new
             {
-                Products = products.ToList(),
+                Products = products,
                 CartItemCount = cartItems.Count()
             };
 
