fb11252c5e09b98288292b19d82388279d96e462
NoReply Amazon <no-reply@amazon.com>
2025-04-16 10:51:59 +0000

    Revert "[Commit] Iteration 176 #errors = 21 <= 21."
    This reverts commit e820d3dc55c660aad3ddc31a68d91d2e14ff7c30.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
 
 namespace WebApplication.Controllers
 {
