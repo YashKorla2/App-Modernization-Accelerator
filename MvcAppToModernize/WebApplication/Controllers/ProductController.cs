54b728397c3688ceac4251a99924e17aeef64ed4
NoReply Amazon <no-reply@amazon.com>
2025-04-16 13:56:51 +0000

    Revert "[Commit] Iteration 401 #errors = 21 <= 21."
    This reverts commit ead64dfcb434708588925e168a7b8aedf07b23cc.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,9 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
-using Models;
 using Services;
+using Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace WebApplication.Controllers
 {
