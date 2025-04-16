cce559a49906561334caec43874f6eb0e3897f7f
NoReply Amazon <no-reply@amazon.com>
2025-04-16 13:32:44 +0000

    Revert "[Commit] Iteration 346 #errors = 21 <= 21."
    This reverts commit a3c9f93993579539bd79575b2ac840fceaf47735.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -2,8 +2,6 @@ using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace WebApplication.Controllers
 {
