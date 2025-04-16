6e8a49016fdebd4faa2113d1c6b196120a8c2913
NoReply Amazon <no-reply@amazon.com>
2025-04-16 14:31:04 +0000

    Revert "[Commit] Iteration 477 #errors = 21 <= 21."
    This reverts commit 63a4242faeac487fcb4f5b3c172e9bec5b18b388.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,9 +1,7 @@
 using Services;
 using Models;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace WebApplication.Controllers
 {
@@ -55,7 +53,7 @@ namespace WebApplication.Controllers
         }
 
         [HttpPost("Create")]
-        public ActionResult Create(Product product)
+        public IActionResult Create(Product product)
         {
             if (ModelState.IsValid)
             {
