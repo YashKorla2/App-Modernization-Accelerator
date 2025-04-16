eb527f40bc9c9410ca9d8a65159c969b0343a11f
NoReply Amazon <no-reply@amazon.com>
2025-04-16 11:58:32 +0000

    [Commit] Iteration 284 #errors = 21 <= 21.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0518][71,17] Predefined type 'System.Object' is not defined or imported``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
-using Services;
-using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
-using System.Collections.Generic;
+using Models;
+using Services;
 
 namespace WebApplication.Controllers
 {
