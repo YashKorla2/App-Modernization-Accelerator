927e5678e9405f86aee2c10bae9875240407cb57
NoReply Amazon <no-reply@amazon.com>
2025-04-23 15:38:56 +0000

    Revert "[Commit] Iteration 169 #errors = 31 <= 30."
    This reverts commit 09718bbfe06341de4fea8b1f48bdb76f6d7c4b65.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Models.Entities; // Assuming Cart is in this namespace
 
 namespace WebApplication.Controllers
 {
