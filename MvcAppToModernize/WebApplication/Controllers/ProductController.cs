31eb66ebe446ce48f4a16021f820b5a9113908c9
NoReply Amazon <no-reply@amazon.com>
2025-04-23 15:20:44 +0000

    Revert "[Commit] Iteration 134 #errors = 29 <= 29."
    This reverts commit f5780844af2ea27eed2cfd818187de5eb6c8766f.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -6,7 +6,6 @@ using Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Http;
-using System;
 
 namespace WebApplication.Controllers
 {
