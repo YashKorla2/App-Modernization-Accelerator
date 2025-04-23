3109fb09b4488ed6dd0effd4b87c6683d1c41f40
NoReply Amazon <no-reply@amazon.com>
2025-04-23 20:41:51 +0000

    Revert "[Commit] Iteration 596 #errors = 27 <= 27."
    This reverts commit 76d0e609e415e8efddc2fc957825fbdda292cc9b.

--- MvcAppToModernize/WebApplication/Controllers/OrderController.cs
+++ MvcAppToModernize/WebApplication/Controllers/OrderController.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Services;
 using Microsoft.AspNetCore.Mvc;
-using Models; // Add this line to ensure Order type is recognized
 
 
 namespace WebApplication.Controllers
