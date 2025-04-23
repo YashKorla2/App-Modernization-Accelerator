b990822428dcb1e9cb22b58977216274a72f4398
NoReply Amazon <no-reply@amazon.com>
2025-04-23 17:18:56 +0000

    Revert "[Commit] Iteration 306 #errors = 29 <= 28."
    This reverts commit e9040b9ec479208dc7d9834e66fd2f0bd83fed26.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -6,8 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
-using System.Object;
-using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication.Controllers
 {
