34642bbca3a800bedc520e08a6516e2c9ed6ea5b
NoReply Amazon <no-reply@amazon.com>
2025-04-23 13:27:04 +0000

    Revert "[Commit] Iteration 38 #errors = 30 <= 30."
    This reverts commit 43e1461108743046fc470e325af0e95d3b781b28.

--- MvcAppToModernize/WebApplication/Controllers/CartController.cs
+++ MvcAppToModernize/WebApplication/Controllers/CartController.cs
@@ -1,6 +1,5 @@
 using Services;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 
 
 namespace WebApplication.Controllers
@@ -30,13 +29,13 @@ namespace WebApplication.Controllers
         /// <returns>View displaying cart items, filtered by search term if provided</returns>
         public ActionResult Index(string searchTerm)
         {
-            var carts = string.IsNullOrEmpty(searchTerm)
+            var Carts = string.IsNullOrEmpty(searchTerm)
                 ? _cartService.GetCarts()
                 : _cartService.SearchCart(searchTerm);
 
             ViewBag.SearchTerm = searchTerm;
 
-            return View(carts);
+            return View(Carts);
         }
 
         /// <summary>
