f505059b73229e975aa69f772b8333a4788cc4a1
NoReply Amazon <no-reply@amazon.com>
2025-04-23 18:24:35 +0000

    Revert "[Commit] Iteration 378 #errors = 30 <= 30."
    This reverts commit 75a3bedba6a76475af7f0c74bf453edf352be2a4.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -3,7 +3,6 @@ using Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using Models;  // Ensure Models namespace is explicitly imported
 
 namespace WebApplication.Controllers
 {
