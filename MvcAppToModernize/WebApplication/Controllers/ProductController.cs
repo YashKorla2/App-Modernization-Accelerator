8a56fbf3f91083a7217dc8e7e4a16e97672bb0f4
NoReply Amazon <no-reply@amazon.com>
2025-04-23 20:39:53 +0000

    [Commit] Iteration 591 #errors = 29 <= 27.
    Addressing `BuildError `[/tmp/tempfspartition/QnetTransformArtifacts/MidTransformCode/sourceCode/MvcAppToModernize/WebApplication/Controllers/ProductController.cs][CS0246][39,43] The type or namespace name 'List<>' could not be found (are you missing a using directive or an assembly reference?)``.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,10 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Models;
 using Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApplication.Controllers
 {
