d8c5be51dadeb723f3d4cfd91053019eff045a30
NoReply Amazon <no-reply@amazon.com>
2025-04-23 15:11:39 +0000

    Revert "[Commit] Iteration 117 #errors = 29 <= 29."
    This reverts commit 2d22fff9cf46ec617e89bcde78484beda716e61a.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -39,10 +39,10 @@ namespace WebApplication.Controllers
         [HttpGet]
         public ActionResult<ProductViewModel> Index(string searchTerm)
         {
-            List<Product> products = string.IsNullOrEmpty(searchTerm)
-                ? _productService.GetAllProducts().ToList()
-                : _productService.SearchProducts(searchTerm).ToList();
-            List<Cart> cartItems = _cartService.GetCarts().ToList();
+            var products = string.IsNullOrEmpty(searchTerm)
+                ? _productService.GetAllProducts()
+                : _productService.SearchProducts(searchTerm);
+            var cartItems = _cartService.GetCarts();
 
             var viewModel = new ProductViewModel
             {
