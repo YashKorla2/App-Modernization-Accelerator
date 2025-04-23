b1bfa25a5cc003591d2ddd58d212c2674144bbfb
NoReply Amazon <no-reply@amazon.com>
2025-04-23 18:10:45 +0000

    Revert "[Commit] Iteration 350 #errors = 28 <= 28."
    This reverts commit da16ea0431ab0d941c17aeb253beb0c1fd0d311d.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -1,11 +1,11 @@
+using Services;
+using Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Net;
-using Microsoft.AspNetCore.Mvc;
-using Models;
-using Services;
 
 namespace WebApplication.Controllers
 {
@@ -13,7 +13,7 @@ namespace WebApplication.Controllers
     /// Controller responsible for handling all product-related operations including
     /// viewing, creating, editing, deleting products and managing shopping cart
     /// </summary>
-    public class ProductController : Microsoft.AspNetCore.Mvc.Controller
+    public class ProductController : Controller
     {
         private readonly IProductService _productService;
         private readonly ICartService _cartService;
