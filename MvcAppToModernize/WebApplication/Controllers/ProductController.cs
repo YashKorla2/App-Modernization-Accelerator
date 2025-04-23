50882dceeaffa6d4fd03c39be04599ee0658615f
NoReply Amazon <no-reply@amazon.com>
2025-04-23 13:11:23 +0000

    Revert "[Commit] Iteration 5 #errors = 32 <= 30."
    This reverts commit e42bd1bd1ad36143bd2d9e3cd2e8c60ef0e42abd.

--- MvcAppToModernize/WebApplication/Controllers/ProductController.cs
+++ MvcAppToModernize/WebApplication/Controllers/ProductController.cs
@@ -34,15 +34,15 @@ namespace WebApplication.Controllers
         /// </summary>
         public ActionResult Index(string searchTerm)
         {
-            IEnumerable<Product> products = string.IsNullOrEmpty(searchTerm)
+            var products = string.IsNullOrEmpty(searchTerm)
                 ? _productService.GetAllProducts()
                 : _productService.SearchProducts(searchTerm);
-            IEnumerable<Cart> cartItems = _cartService.GetCarts();
+            var cartItems = _cartService.GetCarts();
 
             var viewModel = new ProductViewModel
             {
                 Products = products,
-                CartItemCount = cartItems.Any() ? cartItems.Count() : 0
+                CartItemCount = cartItems.Count
             };
 
             ViewBag.SearchTerm = searchTerm;
